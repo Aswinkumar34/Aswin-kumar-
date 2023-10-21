2068dd2dd32e16e6a13c06a3e8d0a1d615a297fb
Aswinkumar34 <148629968+Aswinkumar34@users.noreply.github.com>
2023-10-21 18:20:21 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+def factorial(n):
+  if n == 0 or n == 1:
+    return 1
+  else:
+    return n * factorial(n - 1)
+
+
+number = int(input('enter a number:'))
+result = factorial(number)
+print(f"The factorial of {number} is {result}")
